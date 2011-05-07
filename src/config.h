d2c9bc916557ddcee9339e4f5c745e5aa4204941
Margaret Wang <pipituliuliu@gmail.com>
2011-05-08 01:13:11 +0800

    license declaration added

--- src/config.h
+++ src/config.h
@@ -1,3 +1,24 @@
+/**
+ * Twilc is an NCURSES based CLI twitter client for Linux.
+ * Copyright (C) 2011 Margaret Wang (pipituliuliu@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ **/
+
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
--- src/twitter.h
+++ src/twitter.h
@@ -1,3 +1,24 @@
+/**
+ * Twilc is an NCURSES based CLI twitter client for Linux.
+ * Copyright (C) 2011 Margaret Wang (pipituliuliu@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ **/
+
+
 #ifndef TWITTER_H
 #define TWITTER_H
 
@@ -20,6 +41,8 @@ typedef struct status_str{
     // position to show
     int y_min;
     int y_max;
+
+    uint8_t extra_info;
 
     char *filtered_text[MAX_FILTERS_PER_STATUS];
     display_filter *filter_list[MAX_FILTERS_PER_STATUS];
